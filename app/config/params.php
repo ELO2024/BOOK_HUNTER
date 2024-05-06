dd39ff268ce392ea49597fe151704f60bd68fcc9
Elo <violette.magdeleine@hotmail.com>
2024-05-06 19:56:01 +0200

    Structure MVC Initiale

---
+++ app/config/params.php
@@ -0,0 +1,9 @@
+<?php
+// PARAMS
+
+// paramètre de connexion à la db
+
+define('DB_HOST', 'localhost:3306');
+define('DB_NAME', 'book_hunter_2023');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'root');
---
+++ core/init.php
@@ -0,0 +1,9 @@
+<?php
+// Initialisation
+
+// 1. Charger les Params
+require_once '../app/config/params.php';
+
+var_dump($connexion);
+// 2. Charger la connexion
+require_once '../core/connexion.php';
