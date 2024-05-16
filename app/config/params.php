e7be513099874faf3f6291104e189421c17d7b6b
Elo <violette.magdeleine@hotmail.com>
2024-05-16 15:21:56 +0200

    Intégration Template + partials

--- app/config/params.php
+++ app/config/params.php
@@ -3,6 +3,10 @@
 
 // paramètre de connexion à la db
 
+// Initialisation des zones dynamique
+$title = '';
+$content = '';
+
 define('DB_HOST', 'localhost:3306');
 define('DB_NAME', 'book_hunter_2023');
 define('DB_USER', 'root');
--- core/init.php
+++ core/init.php
@@ -4,6 +4,6 @@
 // 1. Charger les Params
 require_once '../app/config/params.php';
 
-var_dump($connexion);
+
 // 2. Charger la connexion
 require_once '../core/connexion.php';
